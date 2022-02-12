521d517bef94786b7a7ce2f4355e3df48235aef5
Enrique <enriquer@tenantevaluation.com>
2022-02-12 17:52:08 +0100

    add css in register. add enviroment variables

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,13 +1,14 @@
 import axios from 'axios';
-const API_URL = 'http://localhost:8080/api/auth/';
+const API_URL = process.env.VUE_APP_URL_API;
 class AuthService {
   login(user) {
     return axios
-      .post(API_URL + 'signin', {
+      .post(API_URL + 'login', {
         username: user.username,
         password: user.password
       })
       .then(response => {
+        console.log(response);
         if (response.data.accessToken) {
           localStorage.setItem('user', JSON.stringify(response.data));
         }
